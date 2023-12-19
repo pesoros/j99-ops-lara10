b23ea82b252e8ef4ffcc5c418a649387f7c9aabd
Bayu Yuhartono <bayuyuhartono@gmail.com>
2023-12-19 09:12:34 +0700

    web badge

--- resources/views/layouts/shared/footer.blade.php
+++ resources/views/layouts/shared/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; {{ date('Y') }} Juragan 99 Trans</a>
+    <strong>Copyright &copy; {{ date('Y') }} Juragan 99 Trans</a></strong>
     <div class="float-right d-none d-sm-inline-block">
+        <span class="badge badge-warning">Operasional</span>
         <b>Version</b> 2.0.0
     </div>
 </footer>
