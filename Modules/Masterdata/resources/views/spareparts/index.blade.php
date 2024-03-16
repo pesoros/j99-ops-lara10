817371c123c72e8477ea668c5517c5d70868a35e
Bayu Yuhartono <bayuyuhartono@gmail.com>
2024-03-17 00:54:56 +0700

    refreshtoken button on spareparts masterdata

--- Modules/Masterdata/resources/views/spareparts/index.blade.php
+++ Modules/Masterdata/resources/views/spareparts/index.blade.php
@@ -7,7 +7,7 @@
         <h3 class="card-title">List {{ $title }}</h3>
         <div class="float-right">
           @if (permissionCheck('add'))
-            <a href="{{ url('api/accurate/newtoken') }}" target="_blank" class="btn btn-secondary btn-sm">
+            <a href="{{ url('api/accurate/refreshtoken') }}" target="_blank" class="btn btn-secondary btn-sm">
               Refresh token accurate 
             </a>
           @endif
