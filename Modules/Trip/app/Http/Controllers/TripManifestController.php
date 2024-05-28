7e6955a50f38f57b4082295fe3da3382f170160a
paratonsp <paratonsp@gmail.com>
2024-05-28 10:19:44 +0700

    fix: detail manifest

--- Modules/Trip/app/Http/Controllers/TripManifestController.php
+++ Modules/Trip/app/Http/Controllers/TripManifestController.php
@@ -25,7 +25,8 @@ class TripManifestController extends Controller
         $data['detailManifest'] = Trip::getManifest($id);
         // $data['passengerList'] = Trip::getPassengerList($data['detailManifest']->trip_assign, $data['detailManifest']->trip_date);
 
-        echo($data['detailManifest']);
+        // echo($data['detailManifest']);
+        echo("<script>console.log('PHP: " . $data['detailManifest'] . "');</script>");
 
         // return view('trip::manifest.detail', $data);
     }
