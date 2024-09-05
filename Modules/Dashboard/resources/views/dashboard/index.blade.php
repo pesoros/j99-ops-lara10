fa39974cb393a4dbbff7613f3502cb38fe8981f4
Paraton Suryo Prayogo <70687618+paratonsp@users.noreply.github.com>
2024-09-05 09:07:21 +0700

    test

--- Modules/Dashboard/resources/views/dashboard/index.blade.php
+++ Modules/Dashboard/resources/views/dashboard/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row mb-2">
     <div class="col-sm-12">
-        <h2>Selamat Datang: {{ auth()->user()->name }} | {{ auth()->user()->email }} | {{ Session('role_info_session')->title }}</h2>
+        <h2>Welcome: {{ auth()->user()->name }} | {{ auth()->user()->email }} | {{ Session('role_info_session')->title }}</h2>
     </div>
 </div>
  
