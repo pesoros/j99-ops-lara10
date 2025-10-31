7024d2e087f5a1511a7a894f676bc55aa52480c1
bayuyuhartono <bayuyuhartono@gmail.com>
2025-10-31 16:10:00 +0700

    hotfix: manifest query

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -69,35 +69,26 @@ class Trip extends Model
 
     public function scopeGetPassengerList($query, $tripAssign, $booking_date)
     {
-        $query = DB::table('trip_assign as tras')
+        return DB::table('trip_assign as tras')
             ->select(
-                'tps.name',
-                'tps.ticket_number',
-                'tbook.booking_date',
-                'tbook.price',
-                'tbook.adult',
-                'tps.seat_number',
-                'tps.phone',
-                'tps.food_served',
-                'tbook.pickup_trip_location',
-                'tbook.drop_trip_location',
-                'tbookhead.booking_code',
-                'tbookhead.created_at',
-                'tbookhead.note as note',
-                'tpoint.dep_time',
-                'tpoint.arr_time',
-                'ftype.type as class',
+                'tps.name', 'tps.ticket_number', 'tbook.booking_date', 'tbook.price',
+                'tbook.adult', 'tps.seat_number', 'tps.phone', 'tps.food_served',
+                'tbook.pickup_trip_location', 'tbook.drop_trip_location',
+                'tbookhead.booking_code', 'tbookhead.created_at', 'tbookhead.note as note',
+                'tpoint.dep_time', 'tpoint.arr_time', 'ftype.type as class',
                 'resto.food_name',
                 'paymentreg.payment_channel_code as channel',
                 'arrival_loc.ttpg_id',
                 'rem.is_succeed as reminderSucceed'
             )
-            ->join('tkt_booking as tbook', 'tbook.trip_id_no', 'tras.trip')
+            ->join('tkt_booking as tbook', 'tbook.trip_id_no', '=', 'tras.trip')
             ->join('tkt_booking_head AS tbookhead', 'tbookhead.booking_code', '=', 'tbook.booking_code')
-            ->join('tkt_passenger_pcs as tps', 'tps.booking_id', 'tbook.id_no')
+            ->join('tkt_passenger_pcs as tps', 'tps.booking_id', '=', 'tbook.id_no')
             ->join('fleet_type as ftype', 'ftype.id', '=', 'tps.fleet_type')
-            ->leftJoin('trip_point AS tpoint', function($leftJoin) {
-                $leftJoin->on('tpoint.trip_assign_id', '=', 'tras.id')
+
+            // LEFT JOINs
+            ->leftJoin('trip_point AS tpoint', function($join) {
+                $join->on('tpoint.trip_assign_id', '=', 'tras.id')
                     ->on('tpoint.dep_point', '=', 'tbook.pickup_trip_location')
                     ->on('tpoint.arr_point', '=', 'tbook.drop_trip_location');
             })
@@ -105,14 +96,14 @@ class Trip extends Model
             ->leftJoin('payment_registration AS paymentreg', 'paymentreg.booking_code', '=', 'tbookhead.booking_code')
             ->leftJoin('trip_location as arrival_loc', 'arrival_loc.name', '=', 'tbook.drop_trip_location')
             ->leftJoin('tkt_reminder as rem', 'rem.ticket_number', '=', 'tps.ticket_number')
-            ->where('tbookhead.payment_status', 1)
-            ->where('tps.cancel', 0)
-            ->where('tras.id',$tripAssign)
-            ->whereDate('tbook.booking_date',$booking_date)
-            ->orderBy('tps.seat_number','ASC')
-            ->get();
 
-        return $query;
+            // WHERE Clauses (Filtering)
+            // Place the most selective WHERE clauses first (e.g., primary keys)
+            ->where('tras.id', $tripAssign) // Highly selective
+            ->whereDate('tbook.booking_date', $booking_date) // Selective by date
+            ->where('tbookhead.payment_status', 1) // Standard filter
+            ->where('tps.cancel', 0) // Standard filter
+            ->orderBy('tps.seat_number', 'ASC');
     }
 
     public function scopeGetExpensesList($query, $id)
