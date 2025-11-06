983964688e34d9030e4ab84cc3d151881e80a1e2
bayuyuhartono <bayuyuhartono@gmail.com>
2025-11-06 14:29:57 +0700

    hotfix: passengers manifest list

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -127,12 +127,14 @@ class Trip extends Model
             WHERE tbookhead.payment_status = 1
             AND tps.cancel = 0
             AND tras.id = :tripAssign
+            AND tbook.tras_id = :tripAssignBook
             AND DATE(tbook.booking_date) = :booking_date
             ORDER BY tps.seat_number ASC
         ";
 
         return collect(DB::select($query, [
             'tripAssign' => $tripAssign,
+            'tripAssignBook' => $tripAssign,
             'booking_date' => $booking_date,
         ]));
     }
